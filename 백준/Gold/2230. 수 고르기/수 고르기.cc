a8a281f969852813234331900b018495f570413e
42kko <101621646+42kko@users.noreply.github.com>
2024-08-06 23:35:25 +0900

    [Gold V] Title: 수 고르기, Time: 64 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -2,15 +2,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-/*
-문제해석
-N개의 정수로 이루어진 수열 두수의 차이가 M이상이면서 제일 작은 경우
-1,4 1,5 2,5  1,5차이는 M이상지만 크다.
-갯수 차이
-한줄씩 배열의 숫자
-정렬이후에 찾을껀지, 정렬안한상태에서 하나 찾을껀지->O(엔의 엔제곱)
-백억번 초당 1~5억,
-*/
+
 int n, m, tmp, s = 0, e = 0;
 vector<int> v;
 int ret = 2147483647;
@@ -24,20 +16,17 @@ int main ()
         v.push_back(tmp);
     }
     sort(v.begin(), v.end());
-    for (int i = 0; i < n; i++)
+    while(e < v.size())
     {
-        if (ret == m)
-            break;
-        while(e < v.size())
+        if (ret == m) break;
+        if (v[e] - v[s] >= m)
         {
-            if (v[e] - v[s] >= m)
-            {
-                ret > v[e] - v[s] ? ret = v[e] - v[s] : 0;
-                break;
-            }
-            e++;
+            ret > v[e] - v[s] ? ret = v[e] - v[s] : 0;
+            s++;
         }
-        s++;
+        else
+            e++;
     }
+
     cout << ret;
 }
