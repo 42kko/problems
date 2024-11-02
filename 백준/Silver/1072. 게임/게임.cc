7002e24631e7781223f509be9c7eadbc703a08dc
42kko <101621646+42kko@users.noreply.github.com>
2024-11-02 14:43:39 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -9,12 +9,12 @@ using namespace std;
 게임횟수와 이긴게임을 1씩더해 퍼센트가 바뀌는 최솟값출력
 */
 
-double x, y;
+long long x, y;
 long long initial;
 
-bool change(long long mid, long long initial) {
-    long long newPercent = ((y + mid) * 100) / (x + mid);
-    return newPercent > initial;
+bool change(long long mid) {
+    long long newInit = ((y + mid) * 100) / (x + mid);
+    return !(newInit == initial); //1이면 변화가있다
 }
 
 int main() {
@@ -26,17 +26,18 @@ int main() {
         return 0;
     }
 
-    long long left = 1, right = 1e9, result = -1;
+    long long left = 1, right = 1e9, ans = -1;
     while (left <= right) {
         long long mid = (left + right) / 2;
-        if (change(mid, initial)) {
-            result = mid;  // 최소 게임 횟수 갱신
-            right = mid - 1;  // 더 작은 범위를 탐색
-        } else {
-            left = mid + 1;  // 더 큰 범위를 탐색
+        if (change(mid)) {
+            ans = mid;
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
         }
     }
 
-    cout << result << endl;
+    cout << ans;
     return 0;
 }
