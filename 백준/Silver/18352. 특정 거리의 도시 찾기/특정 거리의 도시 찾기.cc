4fd128e46a86a91f67642d14c70330b321759924
42kko <101621646+42kko@users.noreply.github.com>
2024-11-06 19:40:52 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 352 ms, Memory: 21316 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,49 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
-/*
-단방향 그래프가 주어질때 거리계산
-bfs로 풀리는지 모르겠음
-안풀릴 이유는 없는거같은데
-
-시간 복잡도는 괜찮을지 의문
-*/
-
-vector<int> v[1000005];
-int vist[300005] = {0,};
+vector<int> v[300005];
+int vist[300005]; // 방문 여부와 거리를 저장
 int n, m, k, x;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
     cin >> n >> m >> k >> x;
     for (int i = 0; i < m; i++) {
         int tmp, tmp1;
         cin >> tmp >> tmp1;
         v[tmp].push_back(tmp1);
     }
-    
+
     queue<int> q;
     q.push(x);
-    vist[x] = 1;
+    vist[x] = 1; // 시작 노드의 거리는 1 (방문 표시)
+
+    vector<int> result; // 거리 k인 노드를 저장
+
     while (!q.empty()) {
         int cur = q.front();
         q.pop();
         for (const int e : v[cur]) {
-            if (vist[e]) continue;
-            q.push(e);
-            vist[e] = vist[cur] + 1;
+            if (vist[e]) continue; // 이미 방문한 노드는 스킵
+            vist[e] = vist[cur] + 1; // 거리 갱신
+            if (vist[e] - 1 == k) { // 거리 k인 노드
+                result.push_back(e);
+            } else if (vist[e] - 1 < k) { // 거리 k 이하인 경우만 큐에 삽입
+                q.push(e);
+            }
         }
     }
 
-    int flag = 0;
-    for (int i = 1; i <= n; i++) {
-        if (vist[i] - 1 == k) {
-            cout << i << "\n";
-            flag = 1;
+    if (result.empty()) {
+        cout << "-1\n"; // 거리 k인 노드가 없는 경우
+    } else {
+        // 결과를 오름차순으로 출력
+        sort(result.begin(), result.end());
+        for (int r : result) {
+            cout << r << "\n";
         }
     }
-    if (!flag)
-        cout << "-1";
+
+    return 0;
 }
