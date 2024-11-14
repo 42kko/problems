51c15b3d014c656a252e79fbc9c8926991961fa7
42kko <kko@student.42seoul.kr>
2024-11-14 21:48:56 +0900

    [level 3] Title: 단속카메라, Time: 3.74 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int solution(vector<vector<int>> routes) {
     // 차량의 진출 지점을 기준으로 오름차순 정렬
-    sort(routes.begin(), routes.end(), [](const vector<int>& a, const vector<int>& b) {
+    sort(routes.begin(), routes.end(), [](vector<int> a, vector<int> b) {
         return a[1] < b[1];
     });
     
@@ -14,9 +14,8 @@ int solution(vector<vector<int>> routes) {
     int camera = -30001; // 카메라의 초기 위치를 가능한 최소값보다 작게 설정
     
     for (const auto& route : routes) {
-        // 차량의 진입 지점이 현재 카메라 위치보다 크면 카메라를 설치해야 함
         if (route[0] > camera) {
-            camera = route[1]; // 해당 차량의 진출 지점에 카메라 설치
+            camera = route[1];
             answer++;
         }
     }
