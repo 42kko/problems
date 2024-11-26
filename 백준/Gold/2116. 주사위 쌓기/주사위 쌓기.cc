4062ac91b1746d24471c2d79ae40bee3e2d4d31c
42kko <kko@student.42seoul.kr>
2024-11-26 10:47:18 +0900

    [Gold V] Title: 주사위 쌓기, Time: 8 ms, Memory: 2784 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.cc
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.cc
@@ -1,58 +1,73 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
-// 주사위의 맞은편 면을 찾기 위한 매핑
-int getOpposite(int index) {
-    switch (index) {
-        case 0: return 5;
-        case 1: return 3;
-        case 2: return 4;
-        case 3: return 1;
-        case 4: return 2;
-        case 5: return 0;
+/*
+처음 시작값 6개의 값을 하나씩 돌려가면 모든 문제 탐색
+정육면체의 값을 어떻게 받아올지
+
+다음 주사위를 올릴때 가장큰값이 올라오도록 올린다.
+A F, B D, C E 가 짝을지어 바닥에 놓일수있다.
+*/
+
+int n;
+vector<int> v[10005];
+
+int opposite(int idx) {
+    switch (idx) {
+    case 0:
+        return 5;
+    case 1:
+        return 3;
+    case 2:
+        return 4;
+    case 3:
+        return 1;
+    case 4:
+        return 2;
+    case 5:
+        return 0;
     }
-    return -1; // 이 값은 실행되지 않음
+    return -1;
+}
+
+int greedy(int nu, int bot, int top) {
+    int ret = 0;
+    for (int i = 0; i < 6; i++) {
+        if (i == bot || i == top) continue;
+        ret = max(v[nu][i], ret);
+    }
+    return ret;
 }
 
 int main() {
-    int n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n;
-    
-    vector<vector<int>> dices(n, vector<int>(6));
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 6; j++) {
-            cin >> dices[i][j];
+        for (int j = 0; j < 6; j++){
+            int tmp;
+            cin >> tmp;
+            v[i].push_back(tmp);
         }
     }
-    
-    int maxSum = 0;
-    
-    // 첫 번째 주사위의 모든 바닥 면 경우를 탐색
-    for (int bottom = 0; bottom < 6; bottom++) {
-        int currentSum = 0;
-        int topValue = dices[0][getOpposite(bottom)]; // 첫 주사위의 천장 값
-        
-        // 각 주사위를 순차적으로 처리
-        for (int i = 0; i < n; i++) {
-            int bottomValue = topValue; // 이전 주사위의 천장이 현재 주사위의 바닥
-            topValue = dices[i][getOpposite(find(dices[i].begin(), dices[i].end(), bottomValue) - dices[i].begin())];
-            
-            // 바닥과 천장을 제외한 옆면의 최댓값 계산
-            int maxSideValue = 0;
-            for (int j = 0; j < 6; j++) {
-                if (dices[i][j] != bottomValue && dices[i][j] != topValue) {
-                    maxSideValue = max(maxSideValue, dices[i][j]);
+
+    int bot, ans = 0;
+    for (int i = 0; i < 6; i++) {
+        int tmp = 0;
+        int top_value = v[0][opposite(i)];
+        tmp += greedy(0, i, opposite(i));
+        for (int j = 1; j < n; j++) {
+            for (int k = 0; k < 6; k++) {
+                if (v[j][k] == top_value) {
+                    bot = k;
+                    break;
                 }
             }
-            currentSum += maxSideValue;
+            top_value = v[j][opposite(bot)];
+            tmp += greedy(j, bot, opposite(bot));
         }
-        
-        maxSum = max(maxSum, currentSum);
+        ans = max(tmp, ans);
     }
-    
-    cout << maxSum << endl;
-    return 0;
+    cout << ans;
 }
