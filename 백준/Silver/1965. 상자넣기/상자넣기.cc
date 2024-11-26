2b05a9d321d7de7c4d448efe7b20c8701462f0d8
42kko <kko@student.42seoul.kr>
2024-11-26 19:18:34 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int board[1005];
+int dp[1005];
+vector<int> sequence;
+int ans;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> board[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (board[i] > board[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        ans = max(ans, dp[i]);
+    }
+    cout << ans + 1;
+}
