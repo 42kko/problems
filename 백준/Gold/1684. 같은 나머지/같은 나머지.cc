93018424b07967a80493601c6c44ad36174e5218
42kko <kko@student.42seoul.kr>
2025-01-08 16:01:05 +0900

    [Gold V] Title: 같은 나머지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1684. 같은 나머지/같은 나머지.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+입력으로 들어오는 수는 어떤 정수 D로 나누었을때 나머지가 같다.
+D중 가장 큰 D를 구하시오
+*/
+
+int n;
+int arr[1005];
+int ans;
+vector<int> v;
+
+int GCD(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int main() {
+    cin >> n;
+    cin >> arr[0];
+    for (int i = 1; i < n; i++) {
+        cin >> arr[i];
+        v.push_back(arr[i] - arr[i - 1]);
+    }
+    sort(v.begin(), v.end());
+    
+    ans = v[0];
+    for (int i = 1; i < v.size(); i++) {
+        ans = GCD(ans, v[i]);
+    }
+    cout << abs(ans);
+}
